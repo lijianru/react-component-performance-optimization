31c469dad1943d55ed3f4f5753083e80a5f9c730
lijianru <2219269374@qq.com>
2019-12-21 10:28:50 +0800

    WPO

--- src/pages/WPO/Lv05_useMemo.js
+++ src/pages/WPO/Lv05_useMemo.js
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react'
+import {expensiveFnCopy} from "../constants";
 
 export default function Demo5() {
   const [num, setNum] = useState(0);
@@ -12,9 +13,10 @@ export default function Demo5() {
     return result;
   }
 
+  const base = expensiveFn()
+  // const base = expensiveFnCopy()
   // 通过useMemo将计算结果缓存起来，避免在组件重新渲染的时候再次执行expensiveFn函数
   // const base = useMemo(expensiveFn, []);
-  const base = expensiveFn()
 
   return (
     <div className="App">
--- src/pages/constants.js
+++ src/pages/constants.js
@@ -3,3 +3,12 @@ export const person ={
     age: 18,
   }
 }
+
+export function expensiveFnCopy() {
+  console.log('expensiveFnCopy')
+  let result = 0;
+  for (let i = 0; i < 10000; i++) {
+    result += i;
+  }
+  return result;
+}
