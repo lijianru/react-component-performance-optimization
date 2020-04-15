29c559c1dca485dbd2a82d7add9856e5412237db
lijianru <2219269374@qq.com>
2020-04-15 10:26:46 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React from 'react';
-import Demo1 from "./pages/WPO/Lv01_shouldComponentUpdate";
-import Demo2 from "./pages/WPO/Lv02_PureComponent";
-import Demo3 from "./pages/WPO/Lv03_memo";
-import Demo4 from "./pages/WPO/Lv04_useCallback";
-import Demo5 from "./pages/WPO/Lv05_useMemo";
+import React from 'react'
 
 function App() {
   return (
-    <div style={{margin: '50px'}}>
+    <div style={{ margin: '50px' }}>
       <h3>Lv01_shouldComponentUpdate</h3>
-      <Demo1/>
-      <hr/>
-      <h3>Lv02_PureComponent</h3>
-      <Demo2/>
-      <hr/>
-      <h3>Lv03_memo</h3>
-      <Demo3/>
-      <hr/>
-      <h3>Lv04_useCallback</h3>
-      <Demo4/>
-      <hr/>
-      <h3>Lv05_useMemo</h3>
-      <Demo5/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
